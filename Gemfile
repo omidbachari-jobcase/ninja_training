23343af30e079f2bfac7087b5535c040fc2c6598
Omid Bachari <obachari@jobcase.com>
2015-09-01 08:28:07 -0400

    added more config

--- Gemfile
+++ Gemfile
@@ -8,11 +8,19 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'httpclient'
 
 group :development, :test do
+  gem 'dotenv-rails'
+  gem 'vcr'
+  gem 'webmock'
   gem 'capybara'
   gem 'rspec-rails', '~> 3.0'
   gem 'pry'
   gem 'web-console', '~> 2.0'
   gem 'spring'
+end
+
+group :test do
+  gem 'coveralls'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'vcr'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -10,4 +11,12 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.hook_into :webmock
+  c.filter_sensitive_data("<API_KEY>") { ENV["API_KEY"] }
+  c.default_cassette_options = { record: :new_episodes }
+  c.configure_rspec_metadata!
 end
